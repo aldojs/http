0d75903959637e2684d531e753f0bf2810341000
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2018-02-04 12:13:51 +0000

    Update support/content-type.js Enhance the "is" function to early return false if the content type header is undefined

--- src/support/content-type.ts
+++ src/support/content-type.ts
@@ -1,10 +1,10 @@
 
-import * as typeis from 'type-is'
+import { is as typeis } from 'type-is'
 
 export function extract (header: string): string {
   return header ? header.split(';', 1)[0].trim() : ''
 }
 
-export function is (value: string, types: string[]) {
-  return typeis.is(value, types)
+export function is (value: string, types: string[]): string | false {
+  return value ? typeis(value, types) : false
 }
