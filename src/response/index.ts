910ef0351c67e9520a50243355a5b317689b71be
absolux <a.mouhassine@gmail.com>
2018-10-31 01:10:13 +0000

    Remove response classes

--- src/response/index.ts
+++
@@ -1,5 +0,0 @@
-
-export * from './factory'
-export * from './stream'
-export * from './json'
-export * from './base'
--- test/response/_support.ts
+++
@@ -1,9 +0,0 @@
-
-import { Response } from '../../src'
-
-/**
- * 
- */
-export function createResponse (body?: any): Response {
-  return new Response(body)
-}
